19b5a05c54bca31942eb78a4fe6e588bd015a6a5
cvellai <cvellai@ncsu.edu>
2018-12-31 19:00:31 -0500

    Setting up repository

---
+++ README.md
@@ -0,0 +1,42 @@
+# Quantum Computing in the Cloud
+A Cloud based solution where Quantum computers are allocated as a container running on VCL (Virtual Computing Lab)
+instance to the user on demand. This container system utilizes Jupyter notebook. IBM Quantum simulators are used to execute
+Quantum algorithms.  
+
+#### Prerequisites 
+
+ * An account must be created and an API token generated from the [IBM Q Experience](https://quantumexperience.ng.bluemix.net/qx/experience) and pasted 
+ in Qconfig.py file.
+  
+Host Machine on which application is tested: Ubuntu 16.04..5 LTS (Xenial Xerus)
+ 
+#### Installation - Following commands are executed in order to set up the infrastructure   
+
+```
+sudo apt-get install docker.io  
+sudo apt-get -y install python3.5 python3-pip python3-dev  
+python3 -m pip install --upgrade pip  
+pip insatll --user flask  
+pip insatll --user pyyaml  
+sudo apt-get install python-yaml  
+sudo groupadd docker  
+sudo usermod -aG docker $USER  
+sudo iptables -I INPUT -p tcp --dport 1000 -j ACCEPT  
+```
+#### Code Files description
+
+ * main.py - Application entry file
+ * Dockerfile - Creates Jupyter container with necessary dependencies  
+ * Qconfig.py - contains the IBM Quantum API tiken  
+ * metadata.yaml - stores details of the container to be created next  
+ * script.py - contains method to create new container  
+ * user_data.json - contains user details  
+ * /templates - Folder contains the html template files  
+ 
+ 
+## Execution  
+
+In your terminal, run the following command to run the application:
+
+```
+sudo python3 main.py
---
+++ main.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, render_template, redirect, url_for
+import json
+import subprocess
+import yaml
+import time
+import socket
+
+app = Flask(__name__)
+
+# Method to return the newly created jupyter container details  
+def create_jupyter_container(ssh_password):
+    python3_command = "python script.py "+ssh_password  # launch your python2 script using bash
+
+    process = subprocess.Popen(python3_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()     
+    new_container_data = json.loads(output.decode("utf-8"))
+    return new_container_data
+
+
+# Method to append new container details in json file
+def insert_new_container_data_for_an_user(user_name, new_container_data):
+    old_data = ''
+    
+    # Append new container data
+    with open('user_data.json') as json_file: 
+        old_data = json.load(json_file)
+        if user_name in old_data:
+            current_data = old_data[user_name]['containers']
+        else:
+            current_data = []
+        current_data.append(new_container_data)
+        old_data[user_name]['containers'] = current_data
+    
+    # Dump updated data in the json file
+    with open('user_data.json', 'w') as outfile:
+        json.dump(old_data,outfile)
+
+    print("New data inserted")
+
+def delete_container_for_an_user(user_name, container_id):
+    old_data = ''
+    subprocess.call(['docker','kill',container_id])
+    with open('user_data.json') as json_file:
+        old_data = json.load(json_file)
+        current_data = old_data[user_name]['containers']
+        index_to_delete = 0
+        for idx, val in enumerate(current_data):
+            if val['containerID'] == container_id:
+                index_to_delete = idx
+        del current_data[index_to_delete]
+        old_data[user_name]['containers'] = current_data
+
+    # Dump updated data in the json file
+    with open('user_data.json', 'w') as outfile:
+        json.dump(old_data,outfile)
+
+@app.route('/')
+def home_page():
+    # Home page with link to login page
+    return render_template('home.html')
+
+@app.route('/user/<username>', methods=['GET', 'POST'])
+def show_user_profile(username):
+    # show the user profile for that user
+    
+    if request.method == 'POST':
+        if "cont_delete" in request.form:
+            delete_container_id = request.form['cont_delete']
+            user_name = request.form['uname']
+            delete_container_for_an_user(user_name, delete_container_id)
+            print("container deleted is", delete_container_id)
+        else:
+            user_name = request.form['uname']
+            ssh_password = request.form['ssh_pass']
+
+            # Create a new Container for this user
+            new_container_data = create_jupyter_container(ssh_password)
+            new_container_data['sshPassword'] = ssh_password
+            # Insert new container details of the user in json file
+            insert_new_container_data_for_an_user(username, new_container_data)
+    
+    # Retrieve all container details from jsom file
+    with open('user_data.json') as json_file: 
+        data = json.load(json_file)
+        user_data = data[username]['containers']
+ 
+    # Return to user details page
+    return render_template('user.html', user_name = username, user_data = user_data)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['uname']
+        password = request.form['passwd']
+
+        # User credential validation
+        with open('user_data.json') as json_file: 
+            data = json.load(json_file)
+            # If username not found then display appropriate message
+            errmsg = ''
+            if user_name not in data:
+                return render_template('login.html', errmsg = 'Username not Found!!')
+            elif data[user_name]['password'] != password:
+                return render_template('login.html', errmsg = 'Incorrect password, Please try again.')
+
+        # If valid username, redirect to user page
+        return redirect(url_for('show_user_profile',username = user_name))
+    else:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(host= '0.0.0.0',port = 1000, debug = True)
